381f82934e05daecbc3d57542625ee2be39ee415
Melissa Preece <melissanchez@gmail.com>
2020-06-01 22:35:41 -0700

    4th Draft
    -Restructured generateEmployeeCard() function by adding click listener to each employee div rather than gallery parent.
    - Issue: div containers are not generating, therefore click listeners not firing

--- public_api_request-v1/js/script.js
+++ public_api_request-v1/js/script.js
@@ -21,9 +21,9 @@ function fetchData(url) {
 
 fetchData("https://randomuser.me/api/?results=12")
     .then(data => {
-        const employeeList = data.results;
-        console.log(employeeList);
+        let employeeList = data.results;
         generateEmployeeCard(employeeList);
+        console.log(employeeList);
     })
     .then(generateSearchBar)
 
@@ -45,51 +45,54 @@ fetchData("https://randomuser.me/api/?results=12")
 
     // Gallery Markup:
     function generateEmployeeCard(data) {
-        const galleryList = data.map(item => 
-            `<div class="card">
-                <div class="card-img-container">
-                    <img class="card-img" src="${item.picture.large}" alt="profile picture">
-                </div>
-                <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
-                    <p class="card-text">${item.email}</p>
-                    <p class="card-text cap">${item.location.city}</p>
-                </div>
-            </div>
-            `).join("");
+        
+        //Create div containers for each employee card:
+        for (var i=0; i<data.length; i++) {
+            const divCard = document.createElement("div");
+            divCard[i].className = "card";
+            gallery.append(divCard[i]);
+        };
 
-        gallery.innerHTML = galleryList;
+        //Loop through fetched data and populate employee card fields:
+        data.forEach(item => {
+            item +=
+                `<div class="card-img-container">
+                        <img class="card-img" src="${item.picture.large}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
+                        <p class="card-text">${item.email}</p>
+                        <p class="card-text cap">${item.location.city}</p>
+                </div>`;
+            divCard.innerHTML += item;
+
+            // Add click event listeners to each employee card:
+            divCard.addEventListener("click", function(event) {
+                event.preventDefault();
+                generateModalWindow(item);
+            });
+        });
     };
 
     // Modal Window Markup:
-    function generateModalWindow(profile) {
-        // Declared variables from selected elements of employee profile
-        let img = profile.querySelector("img");
-        let name = profile.querySelector("h3");
-        let email = profile.querySelector("p.card-text");
-        let city = profile.querySelector("p.card-text.cap");
-
-        // How do I capture the addtl info and populate it into the modal window?
-
-
+    function generateModalWindow(employeeDetail) {
         // Populate the specific user data on to HTML Markup Modal Window
         const modal = `
         <div class="modal-container">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${img.src}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${name.innerHTML}</h3>
-                <p class="modal-text">${email.innerHTML}</p>
-                <p class="modal-text cap">${city.innerHTML}</p>
+                <img class="modal-img" src="${employeeDetail.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${employeeDetail.name.first} ${employeeDetail.name.last}</h3>
+                <p class="modal-text">${employeeDetail.email}</p>
+                <p class="modal-text cap">${employeeDetail.location.city}</p>
                 <hr>
-                <p class="modal-text">(555) 555-5555</p>
-                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                <p class="modal-text">Birthday: 10/21/2015</p>
+                <p class="modal-text">${employeeDetail.phone}</p>
+                <p class="modal-text">${employeeDetail.location}</p>
+                <p class="modal-text">Birthday: ${employeeDetail.dob.date}</p>
             </div>
         </div>
         `;
-
         body.innerHTML = modal;
     };
 
@@ -98,16 +101,16 @@ fetchData("https://randomuser.me/api/?results=12")
 
 ///// EVENT LISTENERS /////
 
-gallery.addEventListener("click", function(event) {
-    event.preventDefault();
+// gallery.addEventListener("click", function(event) {
+//     event.preventDefault();
 
-    if (event.target !== gallery) {
+//     if (event.target !== gallery) {
         // Declare variable to hold 'closest' employee card
-        let card = event.target.closest("div.card");
-        console.log(card);
-        generateModalWindow(card);
-    };
-});
+//         let clicked = event.target.closest("div.card");
+
+//         generateModalWindow(clicked);
+//     };
+// });
 
 
 
